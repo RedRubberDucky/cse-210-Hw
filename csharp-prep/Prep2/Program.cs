7dc62264139687b7e3ce98c9e29d18694ec297ca
IsaacStanworth <sta21021@byui.edu>
2023-04-25 11:09:41 -0600

    Completed prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What was the grade you got on your assignment? ");
+        string percentage = Console.ReadLine();
+        int grade = int.Parse(percentage);
+
+        string total = "";
+
+        if (grade >= 90)
+        {
+            total = "A";
+        }
+        else if (grade < 90 || grade >= 80)
+        {
+            total = "B";   
+        }        
+        else if (grade < 80 || grade >= 70)
+        {
+           total = "C"; 
+        }
+        else if (grade < 70 || grade >= 60)
+        {
+            total = "D";
+        }
+        else 
+        {
+           total = "F"; 
+        }
+        Console.WriteLine($"Your final grade is: {total}");
+        
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratualtions you passed the class!");
+        } 
+        else
+        {
+            Console.WriteLine("Try again");
+        }   
     }
 }
