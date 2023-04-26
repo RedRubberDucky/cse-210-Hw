148da4cd2046e24234dab2bbb22cf37e3adb3192
IsaacStanworth <sta21021@byui.edu>
2023-04-25 19:40:59 -0600

    Completed prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+        
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Your guess is to low. ");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Your guess was to high. ");
+            }
+            else 
+            {
+                Console.WriteLine("You got the right number! ");
+            }
+        }
+
+
+        
+    
+
     }
 }
