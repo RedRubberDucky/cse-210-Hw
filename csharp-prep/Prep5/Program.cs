ec4f2ca55e260d37732cda9af186e124e6525426
IsaacStanworth <sta21021@byui.edu>
2023-04-26 16:28:02 -0600

    Completed prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcomeMessage();
+
+        int favoriteNumber = PromptFavoriteNumber();
+        string personsName = PromptPersonsName();
+
+        int squaredNumber = SquareNumber(favoriteNumber); 
+
+        DisplayResult(personsName, squaredNumber);
+    }
+
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptPersonsName()
+    {
+        Console.Write("Enter your first name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static int PromptFavoriteNumber()
+    {
+        Console.Write("Enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square; 
+    }
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
     }
 }
