ab3d5207dd5dbcd278fd93082d360039e72630c1
IsaacStanworth <sta21021@byui.edu>
2023-04-25 20:21:27 -0600

    Completed prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,34 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers;
+        numbers = new List<int>();
+        
+        int start = -1;
+        while (start != 0)
+        {
+            Console.Write("Add a number to the list (enter 0 to exit). ");
+            
+            string userInput = Console.ReadLine();
+            start = int.Parse(userInput);
+
+            if (start != 0)
+            {
+                numbers.Add(start);
+            }
+            int sum = 0;
+            foreach (int number in numbers)
+            {
+                sum += number; 
+            }
+
+            Console.WriteLine($"The sum is: {sum}");
+
+            float average = ((float)sum) / numbers.Count;
+            Console.WriteLine($"The average is: {average}");
+        }
     }
 }
